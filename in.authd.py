9c85cea544d628f64e63ea74c6b815d3bdc1b133
Ormajollila <sakari@fake.fi>
2016-01-20 19:39:27 +0200

    Cleaning up

--- in.authd.py
+++ in.authd.py
@@ -2,7 +2,7 @@
 
 # Add \r to all prints because original ircd seems to expect it.
 
-import sys, os
+import sys, os, re
 
 def main():
 
@@ -11,27 +11,35 @@ def main():
 	else:
 		query = sys.stdin.readline()
 	
-	query = query.split(',')
+	query = query.strip().split(',')
+	
 	if len(query) == 2:
-		query_local_port = query[0].strip()
-		query_remote_port = query[1].strip()
+		query_local_port = query[0]
+		query_remote_port = query[1]
 	
 		# Accept only int
 		try:
 			query_local_port = int(query_local_port)
 			query_remote_port = int(query_remote_port)
+			
 		except:
 			print "%s , %s : ERROR : INVALID-PORT\r" % (str(query_local_port), str(query_remote_port))
 			sys.exit(1)
 
 		uid = get_connection_uid(query_local_port, query_remote_port)
-		if uid != -1:
+		
+		if uid:
 			username = get_username(uid)
 			
-			if username != -1:
+			if username:
 				print "%s , %s : USERID : UNIX : %s\r" % (str(query_local_port), str(query_remote_port), str(username))
+
 			else:
 				print "%s , %s : ERROR : NO-USER\r" % (str(query_local_port), str(query_remote_port))
+
+		else:
+			print "%s , %s : ERROR : NO-USER\r" % (str(query_local_port), str(query_remote_port))
+			
 	else:
 		print "0 , 0 : ERROR :INVALID-PORT\r"
 
@@ -44,7 +52,7 @@ def main():
 # 
 def get_connection_uid(query_local_port, query_remote_port):
 
-	uid = -1
+	uid = False
 
 	# Try to open connection details.
 	ports = []
@@ -53,30 +61,33 @@ def get_connection_uid(query_local_port, query_remote_port):
 			ports.extend(open("/proc/net/tcp", "r").readlines())
 		if os.path.isfile("/proc/net/tcp6"):
 			ports.extend(open("/proc/net/tcp6", "r").readlines())
+			
 	except:
 		print "%s , %s : ERROR : UNKNOWN-ERROR\r" % (str(query_local_port), str(query_remote_port))
 		sys.exit(1)
 		
 	for line in ports:
 
+
 		# Tidy up line
-		line = line.strip()
-		line = ' '.join(line.split())
+		line = re.sub(r'([a-zA-Z0-9])\s+', r'\1 ', line.strip()).split(' ')
 		
-		line = line.split(' ')
 		if len(line) > 11:
+		
 			if len(line[1].split(":")) > 1:
-				junk, tcp_local_port = line[1].split(':')
-				junk, tcp_remote_port = line[2].split(':')
+				tcp_local_port = line[1].split(':')[1]
+				tcp_remote_port = line[2].split(':')[1]
 	
 				try:
 					tcp_local_port = int(tcp_local_port, 16)
 					tcp_remote_port = int(tcp_remote_port, 16)
+					
 				except:
 					pass
 				
 				if tcp_local_port == query_local_port and tcp_remote_port == query_remote_port:
 					uid = line[7]
+					
 	return uid
 	
 #
@@ -87,7 +98,7 @@ def get_connection_uid(query_local_port, query_remote_port):
 #
 def get_username(uid):
 
-	username = -1
+	username = False
 
 	try:
 		fp = open("/etc/passwd", "r")
